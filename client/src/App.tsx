4a1477cda12fda32dc35492d3272a11d9bfe136d
defpmportal.2.0 <morozov.it2020@gmail.com>
2022-06-21 18:14:44 +0300

    Init react-app ts

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+`
+
+const App: React.FC = () => {
+  return (
+    <Wrapper>
+      App
+    </Wrapper>
+  )
+}
+
+export default App
